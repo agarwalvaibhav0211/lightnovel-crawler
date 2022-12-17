3fee91cdd74a3830a0d8edd30d56ac90034d68c6
freeroamer90 <agarwalvaibhav0211@gmail.com>
2022-12-17 14:25:52 +0530

    docker

--- scripts/entry_point.sh
+++ scripts/entry_point.sh
@@ -25,5 +25,14 @@ function shutdown {
 trap shutdown SIGTERM SIGINT
 
 sleep 30
-exec /usr/bin/python3 -m lncrawl --suppress $@ &
+
+export BOT=discord
+export DISCORD_TOKEN=""
+export LOG_LEVEL=CRITICAL
+export APP_ID=1053554057580662794
+export PUBLIC_KEY=d7f3fc236b5e0c57688a2101ed8fe618fb64f5094362cf49ffe6e3b8be66fc89
+
+echo $DISCORD_TOKEN
+
+exec /usr/bin/python3 -m lncrawl --bot discord --shard-id 0 --shard-count 1 $@ &
 wait ${SUPERVISOR_PID}
